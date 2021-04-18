bc04308db029cc9b23981e935f9af09f8444f352
kyle1elyk <steadkyle@gmail.com>
2021-04-18 00:08:55 -0400

    Fixed client udp

--- src/Client.java
+++ src/Client.java
@@ -17,7 +17,6 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Locale;
-import java.util.Scanner;
 import java.util.TimeZone;
 
 public class Client {
